28c48ef2142e36cc764c58e6ca8e489d6d31b1d7
Adjy Sedar Desir <desiradjysedar@gmail.com>
2021-06-16 00:13:40 -0400

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,26 @@
+# coding:utf-8
+
+from sedar_007 import *
+
+
+sedar_007()
+a=0
+b=1
+i=1
+
+nombre =0
+while nombre == 0:
+    nombre=input("Entrer le nombre (nombre positif): ")
+    nombre=int(nombre)
+if nombre==1:
+    c=1
+
+
+else :
+    while i<nombre:
+        c=a+b
+        a=b
+        b=c
+    
+        i=i+1
+print("Pour le nombre {} la reponse est: {}".format(nombre, c))
